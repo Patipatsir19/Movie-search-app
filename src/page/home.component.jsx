7b9466b499a43e9f7c03d381c8125acceaeb9539
Patipatsir19 <patipat.sir@gmail.com>
2022-11-04 17:46:47 +0700

    make home component

--- src/page/home.component.jsx
+++ src/page/home.component.jsx
@@ -1,4 +1,6 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../component/button/button.component";
 
 import Slider from "../component/Slider/slider.component";
 
@@ -6,6 +8,16 @@ const Home = () => {
   return (
     <Fragment>
       <Slider />
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section_header mb-2">
+            <h2>Trending Movie</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">Show More</OutlineButton>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
